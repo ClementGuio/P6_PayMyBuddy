4f1f7532ed8ba3ade5205ebf28138ccdfc96f2e4
Clément Guio <clementguio@gmail.com>
2022-08-05 21:10:08 +0200

    updated DB changes: new column in payment, company_fee

--- paymybuddy/src/test/java/com/paymybuddy/test/resources/setup_testDatabase.sql
+++ paymybuddy/src/test/java/com/paymybuddy/test/resources/setup_testDatabase.sql
@@ -4,5 +4,5 @@ INSERT INTO account(id,email,pword,firstname,lastname) VALUES(1000002,"two@email
 INSERT INTO link_user(id,id_account,id_friend) VALUES(1000001,1000001,1000002);
 INSERT INTO link_user(id,id_account,id_friend) VALUES(1000002,1000002,1000001);
 
-INSERT INTO transaction(id,id_debitor,id_creditor,tr_datetime,amount) VALUES(1000001,1000001,1000002,'20200101',5.0);
-INSERT INTO transaction(id,id_debitor,id_creditor,tr_datetime,amount) VALUES(1000002,1000002,1000001,'20200101',5.0);
+INSERT INTO payment(id,id_debitor,id_creditor,pay_datetime,amount,company_fee) VALUES(1000001,1000001,1000002,'20200101',5.0,1.0);
+INSERT INTO payment(id,id_debitor,id_creditor,pay_datetime,amount,company_fee) VALUES(1000002,1000002,1000001,'20200101',5.0,1.0);
