23aeab4b009f2ea4a97da480f2fe0b9aca5b9862
Clément Guio <clementguio@gmail.com>
2022-08-05 21:16:15 +0200

    Created PaymentManager and methods to execute payment and manage errors

---
+++ paymybuddy/src/main/java/com/paymybuddy/service/PaymentManager.java
@@ -0,0 +1,54 @@
+package com.paymybuddy.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.paymybuddy.exception.InsufficientBalanceException;
+import com.paymybuddy.exception.NegativeAmountException;
+import com.paymybuddy.model.Account;
+import com.paymybuddy.model.Payment;
+import com.paymybuddy.util.PayMyBuddyUtil.ChargeFeesUtil;
+
+public class PaymentManager {
+	
+	@Autowired
+	AccountService accountService;
+	
+	@Autowired 
+	PaymentService paymentService;
+	
+	public Payment paySomeone(Account debitor, Account creditor, String description, Double amount) throws NegativeAmountException, InsufficientBalanceException{
+		
+		if (!isPossiblePayment(debitor,amount)) {
+			throw new InsufficientBalanceException("Insufficient debitor's balance to pay");
+		}
+		Payment payment = executePayment(debitor, creditor, description,amount);
+		updateBD(debitor,creditor,payment);
+		
+		return payment;
+	}
+	
+	public boolean isPossiblePayment(Account debitor, Double amount) {
+		return debitor.hasBalanceToPay(amount);
+	}
+	
+	public Payment executePayment(Account debitor, Account creditor, String description, Double amount) throws NegativeAmountException{
+		//Prélèvement des frais de l'entreprise
+		Double amountCharged = ChargeFeesUtil.amountAfterCharge(amount);
+		//Création du Payment
+		Payment payment = new Payment(null,debitor.getUserId(),creditor.getUserId(),LocalDateTime.now(),description,amountCharged,ChargeFeesUtil.companyFee(amount));
+		//Update balances
+		debitor.withdrawMoney(amount);
+		creditor.addMoney(amountCharged);
+		
+		return payment;
+	}
+	
+	public void updateBD(Account debitor, Account creditor, Payment payment) {
+		accountService.saveOrUpdateAccount(creditor);
+		accountService.saveOrUpdateAccount(debitor);
+		paymentService.saveOrUpdatePayment(payment);
+	}
+	
+}
